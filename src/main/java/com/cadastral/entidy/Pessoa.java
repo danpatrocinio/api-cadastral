e9417d03227dfae07c50f49d5024c38868addc4e
danpatrocinio <dpatrocinio@gmail.com>
2020-04-09 23:18:02 -0300

    removendo comentarios

--- src/main/java/com/cadastral/entidy/Pessoa.java
+++ src/main/java/com/cadastral/entidy/Pessoa.java
@@ -11,18 +11,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.util.Date;
 
-/* Requisitos:
-
-        Nome - obrigatório
-        Sexo
-        E-mail - não obrigatório, deve ser validado caso preenchido
-        Data de Nascimento - obrigatório, deve ser validada
-        Naturalidade
-        Nacionalidade
-        CPF - obrigatório, deve ser validado (formato e não pode haver dois cadastros com mesmo cpf)
-        Obs: a data de cadastro e atualização dos dados devem ser armazenados.
-*/
-
 @Data
 @NoArgsConstructor
 @Entity
@@ -38,12 +26,12 @@ public class Pessoa extends Auditable<String> {
     private String cpf;
     @NotNull(message = "{Pessoa.nome.NotNull}")
     private String nome;
-    private String sexo;
     @Email(message = "{Generic.invalid.email}")
     private String email;
     @Past(message = "{Pessoa.dtNascimento.Past}")
     @NotNull(message = "{Pessoa.dtNascimento.NotNull}")
     private Date dataNascimento;
+    private String sexo;
     private String naturalidade;
     private String nacionalidade;
 
