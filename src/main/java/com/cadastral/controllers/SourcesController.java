2983110d77724fb220d0eac079c8c760e58f3213
danpatrocinio <dpatrocinio@gmail.com>
2020-04-09 21:50:59 -0300

    adicionando o link para os fontes e database

--- src/main/java/com/cadastral/controllers/SourcesController.java
+++ src/main/java/com/cadastral/controllers/SourcesController.java
@@ -10,7 +10,8 @@ public class SourcesController {
     public String getSourcesLink(){
         return "<p style=\"margin-left:5em; margin-top:3em;\">" +
                 "O código fonte desta API está disponível em: <br><br><a style=\"margin-left:2em;\" href=\"https://github.com/danpatrocinio/api-cadastral\">https://github.com/danpatrocinio/api-cadastral</a>" +
-                "</p>";
+                "<br><br>" +
+                "O database desta API pode ser acessado em: <br><br><a style=\"margin-left:2em;\" href=\"http://localhost:8080/h2\">http://localhost:8080/h2</a> <small style=\"margin-left:1em;\">(senha em branco)</small>";
     }
 
 }
